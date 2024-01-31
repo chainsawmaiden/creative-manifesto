e9fc1b70308bc3f32c11a29330f1c4aad4ff4836
Aditya Das <ziggydas@gmail.com>
2024-01-31 00:08:10 -0500

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,63 @@
-import Image from "next/image";
+'use client'
+
+import Image, { StaticImageData } from "next/image";
+import Draggable, {DraggableCore} from "react-draggable";
+
+import human1 from "@/public/human1.png";
+import human2 from "@/public/human2.png";
+import human3 from "@/public/human3.png";
+import human4 from "@/public/human4.png";
+import human5 from "@/public/human5.png";
+import human6 from "@/public/human6.gif";
+import human7 from "@/public/human7.png";
+import human8 from "@/public/human8.png";
+import human9 from "@/public/human9.png";
+import human10 from "@/public/human10.png";
+import human11 from "@/public/human11.png";
+import human12 from "@/public/human12.png";
+import human13 from "@/public/human13.png";
+import human14 from "@/public/human14.png";
+import human15 from "@/public/human15.png";
+import human16 from "@/public/human16.png";
+import human17 from "@/public/human17.png";
+import human18 from "@/public/human18.png";
+import human19 from "@/public/human19.png";
+import human20 from "@/public/human20.png";
+import human21 from "@/public/human21.png";
+import human22 from "@/public/human22.png";
+import human23 from "@/public/human23.png";
+import human24 from "@/public/human24.png";
+
+import manifesto1 from "@/public/manifesto1.png";
+import manifesto2 from "@/public/manifesto2.png";
+import manifesto3 from "@/public/manifesto3.png";
+import manifesto4 from "@/public/manifesto4.png";
+import manifesto5 from "@/public/manifesto5.png";
+import manifesto6 from "@/public/manifesto6.png";
+import manifesto7 from "@/public/manifesto7.png";
+import manifesto8 from "@/public/manifesto8.png";
+import manifesto9 from "@/public/manifesto9.png";
+import manifesto10 from "@/public/manifesto10.png";
+import manifesto11 from "@/public/manifesto11.png";
+import manifesto12 from "@/public/manifesto12.png";
+import manifesto13 from "@/public/manifesto13.png";
+import manifesto14 from "@/public/manifesto14.png";
+import manifesto15 from "@/public/manifesto15.png";
+import manifesto16 from "@/public/manifesto16.png";
+
+const imgs = [human1, human2, human3, human4, human5, human6, human7, human8, human9, human10, human11, human12, human13, human14, human15, human16, human17, human18, human19, human20, human21, human22, human23, human24,
+manifesto1, manifesto2, manifesto3, manifesto4, manifesto5, manifesto6, manifesto7, manifesto8, manifesto9, manifesto10, manifesto11, manifesto12, manifesto13, manifesto14, manifesto15, manifesto16,] as StaticImageData[];
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <main>
+      <div className='inline-flex justify-center items-center p-12 rounded-full border-black border-2'>
+        <p className="font-arial font-normal text-9xl text-center w-[73.5rem]">This manifesto will change as you see fit.</p>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <Draggable>
+        <Image src={human1} alt='' className='w-80 h-auto cursor-pointer pointer-events-auto'/>
+      </Draggable>
     </main>
   );
 }
